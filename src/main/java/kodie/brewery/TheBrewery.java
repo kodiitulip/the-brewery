9f6df9ebb0abb191792ded63026e9c1c610771ce
Kodie <kodii.tulip@gmail.com>
2023-12-17 20:26:42 -0300

    Initial commit

---
+++ src/main/java/kodie/brewery/TheBrewery.java
@@ -0,0 +1,16 @@
+package kodie.brewery;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TheBrewery implements ModInitializer {
+	public static final String MOD_ID = "brewery";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Heya Partner! Ready to brew some beers?");
+	}
+}
---
+++ src/main/java/kodie/brewery/TheBreweryClient.java
@@ -0,0 +1,10 @@
+package kodie.brewery;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheBreweryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
